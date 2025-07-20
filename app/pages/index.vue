d19ceeae2286875ba22a2edcdd9b20dda6d693f7
Alexandre Hallaine <alexandre@hallaine.com>
2025-07-20 12:49:58 -0400

    feat: new design

---
+++ app/pages/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const { data: home } = await useAsyncData(() => queryCollection('content').path('/').first())
+
+useSeoMeta({
+  title: home.value?.title,
+  description: home.value?.description,
+})
+</script>
+
+<template>
+  <UPageSection orientation="horizontal">
+    <template #body>
+      <ContentRenderer
+        v-if="home"
+        :value="home"
+      />
+    </template>
+    <img
+      src="https://avatars.githubusercontent.com/u/52411215"
+      class="rounded"
+      alt="My GitHub Avatar"
+    >
+  </UPageSection>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
-import tailwindcss from '@tailwindcss/vite'
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/content', '@nuxt/image'],
+  modules: ['@nuxt/eslint', '@nuxt/ui-pro', '@nuxt/content'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   content: {
@@ -10,7 +8,6 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2025-07-15',
   nitro: { cloudflare: { deployConfig: true } },
-  vite: { plugins: [tailwindcss()] },
   eslint: {
     checker: true,
     config: { stylistic: true },
