c084d75431708feb979d747bf0c853e3b94d7c25
Alexandre Hallaine <alexandre@hallaine.com>
2025-07-20 05:31:15 +0200

    feat: eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxt/content'],
   devtools: { enabled: true },
+  content: {
+    experimental: { nativeSqlite: true },
+  },
   compatibilityDate: '2025-07-15',
   eslint: {
     checker: true,
