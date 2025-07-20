be7fbd021480cfa31c0dcfdc90dfe772a9f0b2b1
Alexandre Hallaine <alexandre@hallaine.com>
2025-07-20 05:18:37 +0200

    feat: eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxt/eslint'],
+  devtools: { enabled: true },
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  eslint: {
+    checker: true,
+    config: { stylistic: true },
+  },
 })
